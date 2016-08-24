47b3511ac06acc00eaf23aeffb61878838693dd7
Prashanth Sams <sams.pickoh@gmail.com>
2016-08-24 16:31:38 +0530

    travis update 18

--- protractor-config.js
+++ protractor-config.js
@@ -1,8 +1,8 @@
 exports.config = {
 
   // seleniumServerJar: 'node_modules/grunt-protractor-runner/node_modules/protractor/selenium/selenium-server-standalone-2.52.0.jar',
-  seleniumAddress: 'http://localhost:9515',
-  chromeDriver: 'node_modules/grunt-protractor-runner/node_modules/protractor/selenium/chromedriver_2.21',
+  seleniumAddress: 'http://localhost:9515/wd/hub',
+  // chromeDriver: 'node_modules/grunt-protractor-runner/node_modules/protractor/selenium/chromedriver_2.21',
   // seleniumArgs: ['-Dwebdriver.ie.driver=./node_modules/gulp-protractor/node_modules/protractor/selenium/IEDriverServer.exe'],
   capabilities: {
     browserName: 'chrome',
