1a2544bc17213cd051317ca02f3deb6ae787166d
Prashanth Sams <sams.pickoh@gmail.com>
2016-08-25 11:34:28 +0530

    circleci #2

--- protractor-config.js
+++ protractor-config.js
@@ -14,7 +14,7 @@ chromeDriver: 'node_modules/grunt-protractor-runner/node_modules/protractor/sele
 
   // Capabilities to be passed to the webdriver instance.
   capabilities: {
-    browserName: 'chrome',
+    browserName: 'firefox',
     // shardTestFiles: true,
     // maxInstances: 2
   },
